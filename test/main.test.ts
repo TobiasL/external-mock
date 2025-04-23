5f78dcb558b18383608264584c5b717bccfd1562
Tobias Lindström <me@tobiaslindstrom.se>
2025-04-23 08:27:08 +0200

    Switch to Biome as the linter and formatter. Switch to Vitest for better TypeScript handling. (#5)

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,21 +1,23 @@
-import { createMock, cleanExternalMocks } from "../src/index";
+import { afterEach, expect, test, vi } from "vitest";
+
+import { cleanExternalMocks, createMock } from "../src/index";
 
 afterEach(() => cleanExternalMocks());
 
-it("Mock a POST route and verify that it's called with the correct payload", async () => {
-  const slackHook = jest.fn();
+test("Mock a POST route and verify that it's called with the correct payload", async () => {
+	const slackHook = vi.fn();
 
-  const fakeSlackServer = createMock(5555);
+	const fakeSlackServer = createMock(5555);
 
-  fakeSlackServer.post("/message").spy(slackHook).reply(200, { sent: true });
+	fakeSlackServer.post("/message").spy(slackHook).reply(200, { sent: true });
 
-  await fetch("http://localhost:5555/message", {
-    method: "post",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ text: "Hello World" }),
-  });
+	await fetch("http://localhost:5555/message", {
+		method: "post",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ text: "Hello World" }),
+	});
 
-  expect(slackHook).toBeCalledWith({ text: "Hello World" });
+	expect(slackHook).toBeCalledWith({ text: "Hello World" });
 });
